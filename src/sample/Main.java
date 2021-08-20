3310d49478902aadb8ff10b848431aae98524e4f
antekvesic <57301167+antekvesic@users.noreply.github.com>
2021-08-20 14:41:31 +0200

    Edit Home view

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("view/Home.fxml"));
+        primaryStage.setTitle("Time & Attendance System");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
