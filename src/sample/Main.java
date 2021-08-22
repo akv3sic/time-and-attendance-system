03f3eb34afc7f1a1809729fa6784c7bc60657331
antekvesic <57301167+antekvesic@users.noreply.github.com>
2021-08-22 15:18:37 +0200

    Implement administration button (opens a new window)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/Home.fxml"));
         primaryStage.setTitle("Time & Attendance System");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 575));
         primaryStage.show();
     }
 
---
+++ src/sample/controller/Home.java
@@ -0,0 +1,29 @@
+package sample.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import sample.Main;
+
+import java.io.IOException;
+
+public class Home {
+    @FXML
+    private Button btnAdministration;
+
+
+    public void handleBtnAdministration(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(Main.class.getResource("view/AdminLogin.fxml"));
+        Stage primaryStage = new Stage();
+        primaryStage.setTitle("Time & Attedndance System - Administration");
+        primaryStage.setScene(new Scene(root, 400, 375));
+        // specifies modality
+        primaryStage.initModality(Modality.WINDOW_MODAL); // default
+        primaryStage.show();
+    }
+}
